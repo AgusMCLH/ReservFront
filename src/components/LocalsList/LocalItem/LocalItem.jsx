dc86665fe4d5ea8d0ab48f9dfa8338ef05b39009
Pache <agustindiazmclh@gmail.com>
2024-08-30 15:42:18 -0300

    Style(Local selector): image placeholder and minimalistic styling for the selector

--- src/components/LocalsList/LocalItem/LocalItem.jsx
+++ src/components/LocalsList/LocalItem/LocalItem.jsx
@@ -1,15 +1,34 @@
-const LocalItem = ({ local }) => {
+const LocalItem = ({ local, selectHandeler }) => {
+
+    const image = `https://picsum.photos/100/100?random=${local.id}`;
+    const getSchedule = () => {
+        let schedule = [];
+        for (let day in local.schedule) {
+            if (local.schedule[day].length > 0) {
+                schedule.push(`${day}: ${local.schedule[day][0]}${local.schedule[day][0]>=12?'pm':'am'}-${local.schedule[day][1]}${local.schedule[day][0]>=12?'pm':'am'}`);
+            } else {
+                schedule.push(`${day}: Cerrado`);
+            }
+        }
+        return schedule;
+    }
+    const Schedule = getSchedule();  
     return (
-        <>
-            <h2>{local.name}</h2>
-            <h2>{local.id}</h2>
-            <br/>
-            <br/><br/>
-            <br/><br/>
-            <br/><br/>
-            <br/>
-            <h2>{local.apertureTime} - {local.closingTime}</h2>
-        </>
+        <div className="LocalItem">
+            <div className="LocalItem__selectionButton" onClick={selectHandeler}></div>
+            <img className="LocalItem__image" src={image} alt="local location" />
+            <div className="LocalItem__Info">
+                <p className="LocalItem__Info-LocalName">{local.name}</p>
+                <p className="LocalItem__Info-LocalAddress">{local.address}</p>
+                <div className="LocalItem__Info-LocalSchedule">
+                    <p className="LocalItem__Info-LocalSchedule-Toggler">Horario</p>
+                    <div className="LocalItem__Info-LocalSchedule-Popover">
+                        {Schedule.map((day, index) => <p key={index} className="LocalItem__Info-LocalSchedule-Day">{day}</p>)}
+                    </div>
+                </div>
+                
+            </div>
+        </div>
     );
 }
 export default LocalItem;
--- src/components/LocalsList/LocalsList.jsx
+++ src/components/LocalsList/LocalsList.jsx
@@ -2,10 +2,22 @@ import LocalItem from "./LocalItem/LocalItem";
 import "./css/style.css";
 
 const LocalsList = ({ locals, setFormInfo}) => {
+
+    let localSelected = undefined;
+    const selectHandeler = (e) => {
+        console.log('actual: ',e.target.parentNode);
+        console.log('anterior: ',localSelected);
+        if(localSelected){
+            localSelected.classList.remove('selected');
+        }
+        localSelected = e.target.parentNode;
+        e.target.parentNode.classList.add('selected');
+    }
+
     return (
-        <div className="localList-Container">
+        <div className="localList-Container" >
         {locals.map((local, index) => (
-            <LocalItem key={index} local={local} setFormInfo={setFormInfo} />
+            <LocalItem key={index} local={local} setFormInfo={setFormInfo} selectHandeler={selectHandeler} />
         ))}
         </div>
     );
