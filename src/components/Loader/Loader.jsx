80e31e93cefecb4781bdab22fe8062bbc6e7c231
Pache <agustindiazmclh@gmail.com>
2024-08-29 23:55:51 -0300

    Feat(Loader): Loader implementation

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import './css/style.css';
+
+const Loader = () => {
+    return (
+        <section>
+            <div className="content">
+                <h2>Reserv</h2>
+                <h2>Reserv</h2>
+            </div>
+        </section>
+    );
+}
+    export default Loader;
